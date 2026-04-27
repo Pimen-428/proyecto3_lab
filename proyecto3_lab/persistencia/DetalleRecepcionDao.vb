00b172ab40cb3e97ac79b0f49b8b7c02260f5cd7
Kymdrus <tops2006miguel@gmail.com>
2026-04-27 18:58:58 +0200

    Dao DetalleRecepción falta testing de recepcion y detalle

---
+++ proyecto3_lab/persistencia/DetalleRecepcionDao.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class DetalleRecepcionDao
+    Public Function Insertar(ByVal det As detalle_recepcion) As Integer
+        Dim sql As String
+
+        sql = "INSERT INTO detalle_recepcion (idRecepcion, idSuministro, cantidad) VALUES (" &
+          det.id_recepcion & ", " &
+          det.id_suministro & ", " &
+          det.cantidad & ");"
+
+        Return AgenteBD.ObtenerAgente.Modificar(sql)
+    End Function
+
+
+End Class
